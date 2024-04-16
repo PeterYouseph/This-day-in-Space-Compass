b2c837c7bf84e8f87dad2ffb18622e1d80d8edbb
PeterYouseph <jose_p@ufms.br>
2024-04-16 10:15:05 -0400

    fix: corrige erro de renderização da aplicação Frontend
    corrige paths que estavam errados no index.js

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -11,27 +11,30 @@ const app = express();
 // Configuração do Handlebars como mecanismo de template para renderização de views
 app.engine('.handlebars', exphbs.engine({ extname: '.handlebars' }, { defaultLayout: 'Main' }));
 app.set('view engine', '.handlebars');
-app.set('views', '/frontend/views');
+app.set('views', './views');
 
 // Middleware para parsing de JSON e dados de formulário URL-encoded
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Servindo arquivos estáticos localizados na pasta /src/public
-app.use(express.static('/frontend/public'));
+app.use(express.static('./public'));
 
 // Rota para renderizar a página inicial com os dados recebidos do backend (Teste em desenvolvimento)
 app.get('/', async (req, res) => {
-    try {
-        // Fazendo uma requisição GET para a rota do backend
-        const response = await axios.get('http://localhost:3000/');
+    // try {
+    //     // Fazendo uma requisição GET para a rota do backend
+    //     const response = await axios.get('http://localhost:3000/');
 
-        // Renderizando a página inicial com os dados recebidos do backend
-        res.render('homepage', { data: response.data });
-    } catch (error) {
-        console.error('Erro ao buscar dados do backend:', error);
-        res.status(500).send('Erro ao buscar dados do backend');
-    }
+    //     // Renderizando a página inicial com os dados recebidos do backend
+    //     res.render('homepage', { data: response.data });
+    // } catch (error) {
+    //     console.error('Erro ao buscar dados do backend:', error);
+    //     res.status(500).send('Erro ao buscar dados do backend');
+    // }
+
+    // Renderizando a página inicial sem os dados recebidos do backend
+    res.render('homepage');
 });
 
 // Inicialização do servidor na porta 3001 e Backend na porta 3000
