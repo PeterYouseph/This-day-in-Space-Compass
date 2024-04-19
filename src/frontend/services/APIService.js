6fdb2813d46786e318eac98e8afcbf6d8922b3af
Sarah B <sarah.mbaraldi@gmail.com>
2024-04-19 11:46:42 -0400

    fix: correção de conexão entre os containers

--- src/frontend/services/APIService.js
+++ src/frontend/services/APIService.js
@@ -5,7 +5,7 @@ const axios = require('axios');
 async function fetchDataFromBackend(params) {
     // Try catch para tratar erros de requisição do backends
     try {
-        const response = await axios.get('http://localhost:3000/', { params });
+        const response = await axios.get('http://backend:3000/', { params });
         return response.data;
     } catch (error) {
         throw error;
